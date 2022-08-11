9c745e721cd04f9dd9120581134f0a68a1bf9ff5
GustSilvaJR <gust.offic@gmail.com>
2022-08-11 16:45:45 -0300

    Iniciando projeto To-do list. | Incluindo funcionalidade para criação de um usuário | Criação de um middleware para validar se o usuário existe| Criação de funcionaldiade que imprima os to-dos de um respectivo usuario | Incluindo funcionalidade para criação de um to-do para um determinado usuário

--- src/index.js
+++ src/index.js
@@ -1,29 +1,67 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find(item => item.username == username)
+
+  if(!user){
+    return response.status(400).json("User is not registered");
+  }else{
+    request.user = user;
+  }
+
+  next();
 }
 
+//UTILIZAR SOME OU FIND PARA IMPEDIR CADASTRO DE USUARIOS REPETIDOS (MESMO USERNAME)
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const {name, username} = request.body;
+  
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos:[]
+  }
+
+  users.push(user);
+
+  response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+
+  response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+
+  const { title, deadline } = request.body;
+
+  newTodo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(newTodo);
+
+  response.status(201).json(newTodo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
